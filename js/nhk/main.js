8556c3e028d1490f2dbe85cb6117dfc83d998325
nozomiyamada <y.nozomi320@gmail.com>
2020-02-09 12:17:01 +0700

    update

--- js/nhk/main.js
+++ js/nhk/main.js
@@ -27,7 +27,7 @@ function selectCategory(){
     document.head.appendChild(newscript);
     document.getElementById('selectarticle').innerHTML = "";
     document.getElementById('selectarticle').innerHTML = '<option selected id="firstoption">--- LOADING ---</option>';
-    setTimeout(function(){appendArticle()}, 1000);
+    setTimeout(function(){appendArticle()}, 3000);
 }
 
 function appendArticle(){
